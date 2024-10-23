bf6def3d47318ba56f0de1789216e3c8cc2cb501
Anko2136 <am.kontaratou@gmail.com>
2024-10-23 16:36:30 +0100

    onChange

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,26 @@ import Product from './Product';
 
 function App() {
   const [counter, setCounter] = useState<number>(1)
+  const [txt,setTxt] = useState<string>('')
 
-  const handler = (e:React.MouseEvent<HTMLButtonElement>):void =>{
+  const handler = (e:React.MouseEvent<HTMLButtonElement>):void =>{ //element type + output type
     setCounter(counter + 1)
   }
+
+  const inputChangeHandler = (e:React.ChangeEvent<HTMLInputElement>):void => {
+    setTxt(e.target.value)
+  }
+
   return (
     <div className="App">
       <Product pCode={1} pName= {"Apple"} qnty={10}/>
       <h1>{counter}</h1>
       <hr/>
-      <button onClick={handler}>Increment</button>
+        <button onClick={handler}>Increment</button>
+      <hr/>
+        <p>{txt}</p>
+      <hr/>
+        <input type = "text" onChange={inputChangeHandler}/>
     </div>
   );
 }
