3da4492affee35fbee7dfd94e904afc55106ae6e
Anko2136 <am.kontaratou@gmail.com>
2024-10-23 16:26:18 +0100

    useState()

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 import Product from './Product';
 
 function App() {
+  const [counter, setCounter] = useState<number|string|null>(1)
+
   return (
     <div className="App">
       <Product pCode={1} pName= {"Apple"} qnty={10}/>
+      {counter}
     </div>
   );
 }
