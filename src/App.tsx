fbef68e24c6e9b1dee418d6fdc86c14ba0f2a352
Anko2136 <am.kontaratou@gmail.com>
2024-10-23 16:31:17 +0100

    onClick

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,17 @@ import './App.css';
 import Product from './Product';
 
 function App() {
-  const [counter, setCounter] = useState<number|string|null>(1)
+  const [counter, setCounter] = useState<number>(1)
 
+  const handler = (e:React.MouseEvent<HTMLButtonElement>):void =>{
+    setCounter(counter + 1)
+  }
   return (
     <div className="App">
       <Product pCode={1} pName= {"Apple"} qnty={10}/>
-      {counter}
+      <h1>{counter}</h1>
+      <hr/>
+      <button onClick={handler}>Increment</button>
     </div>
   );
 }
